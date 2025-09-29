cb3ac9a546463dd0053f03c3a0eda0ac0651086b
Nicolas Da Mutten <nicolas@damutten.ch>
2025-09-29 07:25:33 +0200

    feat: add mova-erinnerungsbuch

--- tofu/mariadb-databases.tf
+++ tofu/mariadb-databases.tf
@@ -6,3 +6,11 @@ module "db_tractor_uptime" {
     "tractor_uptime" = module.u_tractor_uptime
   }
 }
+module "db_mova_erinnerungsbuch" {
+  source = "./modules/mariadb-database"
+
+  name = "mova-erinnerungsbuch"
+  grants = {
+    "mova-erinnerungsbuch" = module.u_mova_erinnerungsbuch
+  }
+}
--- tofu/mariadb-users.tf
+++ tofu/mariadb-users.tf
@@ -3,3 +3,9 @@ module "u_tractor_uptime" {
 
   name = "tractor_uptime"
 }
+
+module "u_mova_erinnerungsbuch" {
+  source = "./modules/mariadb-user"
+
+  name = "mova-erinnerungsbuch"
+}
